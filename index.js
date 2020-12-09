1a3147751d196db16df30a25f637ec59cd473567
Jaime Jose Cueva Sánchez <jcueva@jcueva-ibm.local>
2020-12-09 11:50:58 -0500

    cambios

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const proceso = (minutos, minutoActual = 0) => {
     }
 }
 
-const SERVICE_ENDPOINT = "172.21.191.250:9091";
+const SERVICE_ENDPOINT = "172.21.191.250:8022";
 
 log(`Inicio de la instancia`);
 
